7c3573cf41a546debbee6b1ab40a7577178f0117
KarinadelaRiva <164400941+KarinadelaRiva@users.noreply.github.com>
2025-05-20 10:56:42 -0300

    Update Asignatura.java

--- src/main/java/com/apigestionaulas/apigestionaulas/entities/Asignatura.java
+++ src/main/java/com/apigestionaulas/apigestionaulas/entities/Asignatura.java
@@ -1,7 +1,11 @@
 package com.apigestionaulas.apigestionaulas.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
+@Entity
 @Getter
 @Setter
 @ToString
@@ -9,8 +13,33 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class Asignatura {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(
+            name = "nombre_asignatura",
+            nullable = false,
+            length = 50
+    )
     private String nombre;
+
+    @Column(
+            nullable = false,
+            unique = true
+    )
     private int codigo;
-    boolean requiereLaboratorio;
+
+    @Column(
+            nullable = false
+    )
+    private boolean requiereLaboratorio;
+
+    @OneToMany(
+            mappedBy = "asignatura", // Nombre de la propiedad en la clase Inscripcion que hace referencia a Asignatura
+            cascade = CascadeType.ALL, // Permite que se eliminen las inscripciones asociadas a la asignatura
+            fetch = FetchType.LAZY // Carga perezosa para evitar cargar todas las inscripciones al cargar la asignatura
+    )
+    private List<Inscripcion> inscripciones;
 }
