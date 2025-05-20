1b947eaa5860e4a365cbef6f78e99281e82c9d7d
KarinadelaRiva <164400941+KarinadelaRiva@users.noreply.github.com>
2025-05-20 14:54:15 -0300

    Update Inscripcion.java

--- src/main/java/com/apigestionaulas/apigestionaulas/entities/Inscripcion.java
+++ src/main/java/com/apigestionaulas/apigestionaulas/entities/Inscripcion.java
@@ -1,4 +1,54 @@
 package com.apigestionaulas.apigestionaulas.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Inscripcion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+            nullable = false
+    )
+    private Integer cantidadAlumnos;
+    private Integer margenAlumnos;
+    private LocalDate fechaFinInscripcion;
+
+    @Column(
+            nullable = false
+    )
+    private String comision;
+
+    @ManyToOne(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    @JoinColumn(
+            name = "profesor_id",
+            referencedColumnName = "id",
+            nullable = false
+    )
+    private Profesor profesor;
+
+    @ManyToOne(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    @JoinColumn(
+            name = "asignatura_id",
+            referencedColumnName = "id",
+            nullable = false
+    )
+    private Asignatura asignatura;
 }
