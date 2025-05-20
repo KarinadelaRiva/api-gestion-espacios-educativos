79df8e51d04b1c9af4f649a16b1fd650bf51bab0
KarinadelaRiva <164400941+KarinadelaRiva@users.noreply.github.com>
2025-05-20 14:54:07 -0300

    Update Laboratorio.java

--- src/main/java/com/apigestionaulas/apigestionaulas/entities/Laboratorio.java
+++ src/main/java/com/apigestionaulas/apigestionaulas/entities/Laboratorio.java
@@ -1,4 +1,15 @@
 package com.apigestionaulas.apigestionaulas.entities;
 
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Laboratorio extends Aula{
+    private Integer computadoras;
 }
