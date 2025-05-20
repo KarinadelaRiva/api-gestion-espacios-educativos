bf4e2cfcc909a3512a6b3f6c1e158d35cdf6a1c8
KarinadelaRiva <164400941+KarinadelaRiva@users.noreply.github.com>
2025-05-20 14:53:51 -0300

    Update Aula.java

--- src/main/java/com/apigestionaulas/apigestionaulas/entities/Aula.java
+++ src/main/java/com/apigestionaulas/apigestionaulas/entities/Aula.java
@@ -1,9 +1,42 @@
 package com.apigestionaulas.apigestionaulas.entities;
 
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.List;
 
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Inheritance(strategy = InheritanceType.JOINED)
 public class Aula {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+            nullable = false
+    )
+    private Integer numero;
+
+    @Column(
+            nullable = false
+    )
+    private Integer capacidad;
+    private Boolean tieneProyector;
+    private Boolean tieneTV;
+
+    @OneToMany(
+            mappedBy = "aula", // Nombre de la propiedad en la clase Reserva que hace referencia a Aula
+            cascade = CascadeType.ALL, // Permite que se eliminen las reservas asociadas a el aula si se elimina el aula
+            fetch = FetchType.EAGER // Cuando se carga el aula, se cargan todas las reservas asociadas a el aula
+    )
+    private List<Reserva> reservas;
+
+
 }
