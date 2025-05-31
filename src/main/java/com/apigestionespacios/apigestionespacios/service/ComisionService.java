89db4f83606fbce3682a494440f31d067b90f848
KarinadelaRiva <164400941+KarinadelaRiva@users.noreply.github.com>
2025-05-30 23:36:19 -0300

    Update ComisionService.java

--- src/main/java/com/apigestionespacios/apigestionespacios/service/ComisionService.java
+++ src/main/java/com/apigestionespacios/apigestionespacios/service/ComisionService.java
@@ -14,7 +14,7 @@ public class ComisionService {
 
     private final ComisionRepository comisionRepository;
     private final UsuarioService usuarioService;
-    private final CarreraService carreraService;
+//    private final CarreraService carreraService;
 
     @Autowired
     public ComisionService(ComisionRepository comisionRepository, UsuarioService usuarioService) {
@@ -32,17 +32,17 @@ public class ComisionService {
     }
 
     public Comision guardar(Comision comision) {
-        validarProfesor(comision.getProfesor().getId());
-        validarCarrera(comision.getCarrera().getId());
-        validarAsignatura(comision.getCarrera().getId(), comision.getAsignatura().getId());
+//        validarProfesor(comision.getProfesor().getId());
+//        validarCarrera(comision.getCarrera().getId());
+//        validarAsignatura(comision.getCarrera().getId(), comision.getAsignatura().getId());
         return comisionRepository.save(comision);
     }
 
     public Comision actualizar(Long id, Comision nueva) {
         Comision existente = obtenerPorId(id);
-        validarProfesor(nueva.getProfesor().getId());
-        validarCarrera(nueva.getCarrera().getId());
-        validarAsignatura(nueva.getCarrera().getId(), nueva.getAsignatura().getId());
+//        validarProfesor(nueva.getProfesor().getId());
+//        validarCarrera(nueva.getCarrera().getId());
+//        validarAsignatura(nueva.getCarrera().getId(), nueva.getAsignatura().getId());
 
         existente.setCantidadAlumnos(nueva.getCantidadAlumnos());
         existente.setAsignatura(nueva.getAsignatura());
@@ -60,30 +60,30 @@ public class ComisionService {
         comisionRepository.deleteById(id);
     }
 
-    private void validarProfesor(Long idUsuarioProfesor) {
-        Usuario usuario = usuarioService.obtenerPorId(idUsuarioProfesor);
-        if (usuario == null) {
-            throw new RuntimeException("El usuario con ID " + idUsuarioProfesor + " no existe");
-        }
+//    private void validarProfesor(Long idUsuarioProfesor) {
+//        Usuario usuario = usuarioService.obtenerPorId(idUsuarioProfesor);
+//        if (usuario == null) {
+//            throw new RuntimeException("El usuario con ID " + idUsuarioProfesor + " no existe");
+//        }
+//
+//        boolean esProfesor = usuario.getRol().getNombre().equalsIgnoreCase("PROFESOR");
+//
+//        if (!esProfesor) {
+//            throw new RuntimeException("El usuario con ID " + idUsuarioProfesor + " no tiene el rol de profesor");
+//        }
+//    }
 
-        boolean esProfesor = usuario.getRol().getNombre().equalsIgnoreCase("PROFESOR");
+//    private void validarCarrera(Long idCarrera) {
+//        carreraService.obtenerPorId(idCarrera); // Lanza excepción si no existe
+//    }
 
-        if (!esProfesor) {
-            throw new RuntimeException("El usuario con ID " + idUsuarioProfesor + " no tiene el rol de profesor");
-        }
-    }
-
-    private void validarCarrera(Long idCarrera) {
-        carreraService.obtenerPorId(idCarrera); // Lanza excepción si no existe
-    }
-
-    private void validarAsignatura(Long idCarrera, Long idAsignatura) {
-        Carrera carrera = carreraService.obtenerPorId(idCarrera);
-        boolean asignaturaPertenece = carrera.getAsignaturas().stream()
-                .anyMatch(asig -> asig.getId().equals(idAsignatura));
-
-        if (!asignaturaPertenece) {
-            throw new RuntimeException("La asignatura con ID " + idAsignatura + " no pertenece a la carrera con ID " + idCarrera);
-        }
-    }
+//    private void validarAsignatura(Long idCarrera, Long idAsignatura) {
+//        Carrera carrera = carreraService.obtenerPorId(idCarrera);
+//        boolean asignaturaPertenece = carrera.getAsignaturas().stream()
+//                .anyMatch(asig -> asig.getId().equals(idAsignatura));
+//
+//        if (!asignaturaPertenece) {
+//            throw new RuntimeException("La asignatura con ID " + idAsignatura + " no pertenece a la carrera con ID " + idCarrera);
+//        }
+//    }
 }
